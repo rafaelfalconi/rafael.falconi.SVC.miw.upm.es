b9e985b1ab60d22fc94fd0c2db16c79d5cec4758
Rafael Falconí <r.falconi9030@gmail.com>
2017-09-17 16:27:53 +0200

    Creación C31

---
+++ src/main/java/rafael/falconi/SVC/miw/upm/es/ClassC31.java
@@ -0,0 +1,11 @@
+package rafael.falconi.SVC.miw.upm.es;
+
+public class ClassC31 {
+    public String m1() {
+        return "m1";
+    }
+
+    public String m2() {
+        return "m2";
+    }
+}   
