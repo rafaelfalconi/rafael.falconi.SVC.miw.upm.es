d1a6fdd3e6b6e036fefbd10616e00bfbb36dd517
Rafael Falconí <r.falconi9030@gmail.com>
2017-09-17 16:26:56 +0200

    Creación C11Test

--- src/main/java/rafael/falconi/SVC/miw/upm/es/ClassC11.java
+++ src/main/java/rafael/falconi/SVC/miw/upm/es/ClassC11.java
@@ -2,9 +2,10 @@ package rafael.falconi.SVC.miw.upm.es;
 
 public class ClassC11 {
     public String m1() {
-        return"m1";
+        return "m1";
     }
+
     public String m2() {
-        return"m2";
+        return "m2";
     }
 }
